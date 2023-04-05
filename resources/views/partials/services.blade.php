832a041d376ba41098683ba697b501f06d0ab281
MichMav <michmav28@gmail.com>
2023-04-05 17:22:39 +0200

    fixed the services

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -4,40 +4,64 @@
 
     <div class="section-title">
     <h2>Services</h2>
-    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+    <p></p>
     </div>
 
     <div class="row">
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
         <div class="icon"><i class="bi bi-briefcase"></i></div>
-        <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+        <h4 class="title"><a href="">Développement</a></h4>
+        <p class="description">Le concept de développement désigne
+            <strong>
+                l'ensemble des transformations techniques, sociales, territoriales, démographique et culturelles accompagnmat la croissance de la production.
+            </strong>
+            Il traduit l'aspect structurel et qualitatif de la croissance et peut être associé à
+            l'idée de progrès économique et social.
+        </p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
         <div class="icon"><i class="bi bi-card-checklist"></i></div>
-        <h4 class="title"><a href="">Dolor Sitema</a></h4>
-        <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+        <h4 class="title"><a href="">Création des Contenus</a></h4>
+        <p class="description">
+            la création de contenu, c'est quoi?
+            <strong>
+                Il s'agit de produire du contenu qui attire et retient l'attention d'un cible.
+            </strong>
+            La création de contenu permet à une marque de se distnguer à l'aide de textes, de vidéos,
+            de photos et d'infographies, entre autres, qui int une valuer pour cette cible.
+        </p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
         <div class="icon"><i class="bi bi-bar-chart"></i></div>
-        <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-        <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+        <h4 class="title"><a href="">Web Marketing</a></h4>
+        <p class="description">
+            Le web marketing
+            <strong>
+                a pour fonction d'augmenter le trafic et les ventes d'un site internet.
+            </strong>
+            Au carrefour de plusieurs compétences (Web, marketing), le web marketeur élebore, adapte et met en
+            place une stratégie aux besoins et attentes de son entreprise.
+        </p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
         <div class="icon"><i class="bi bi-binoculars"></i></div>
-        <h4 class="title"><a href="">Magni Dolores</a></h4>
-        <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <h4 class="title"><a href="">Designer UI/UX</a></h4>
+        <p class="description">
+            Le designer UX (User eXperience) et le designer UI (User Interface)
+            <strong>
+                et une spécialisation récente du métier de web designer.
+            </strong>
+            Le premier s'intéresse à l'expérience utilisateur, le second à la conception de l'interface produit.
+        </p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
         <div class="icon"><i class="bi bi-brightness-high"></i></div>
-        <h4 class="title"><a href="">Nemo Enim</a></h4>
-        <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-    </div>
-    <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-        <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-        <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-        <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-    </div>
+        <h4 class="title"><a href="">Modélisation</a></h4>
+        <p class="description">
+            La modélisation est la conception et l'utilisation d'un <strong>modèle.</strong>
+            Selon son objectif (représentation simplifiée, compréhension, prédiction) et les moyens
+            utilisés.
+        </p>
     </div>
 
 </div>
