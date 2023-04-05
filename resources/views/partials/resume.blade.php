79d8687e5427e018fefb53212b37d254428c39dd
MichMav <michmav28@gmail.com>
2023-04-05 16:42:16 +0200

    fixed the resume

--- resources/views/partials/resume.blade.php
+++ resources/views/partials/resume.blade.php
@@ -11,52 +11,57 @@
     <div class="col-lg-6" data-aos="fade-up">
         <h3 class="resume-title">Sumary</h3>
         <div class="resume-item pb-0">
-        <h4>Alex Smith</h4>
+        <h4>Mich Mav</h4>
         <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
         <ul>
-            <li>Portland par 127,Orlando, FL</li>
-            <li>(123) 456-7891</li>
-            <li>alice.barkley@example.com</li>
+            <li>39 rue de Kimbangu, Kalamu, Kinshasa</li>
+            <li>(97) 338-5722</li>
+            <li>michmav28@gmail.com</li>
         </ul>
         </div>
 
         <h3 class="resume-title">Education</h3>
         <div class="resume-item">
-        <h4>Master of Fine Arts &amp; Graphic Design</h4>
-        <h5>2015 - 2016</h5>
-        <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+        <h4>Diplôme d'étude en langue française (DELF B2)</h4>
+        <h5>2019</h5>
+        <p><em>Institut française, Kinshasa, RDC</em></p>
         <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
         </div>
         <div class="resume-item">
-        <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-        <h5>2010 - 2014</h5>
-        <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+        <h4>Diplôme en Gestion informatique</h4>
+        <h5>2019 - 2022</h5>
+        <p><em>Institut de Sciences informatiques, Programmation et Analyse, Kinshasa, RDC</em></p>
         <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
         </div>
     </div>
     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-        <h3 class="resume-title">Professional Experience</h3>
+        <h3 class="resume-title">Expérience Professionnelle</h3>
         <div class="resume-item">
-        <h4>Senior graphic design specialist</h4>
-        <h5>2019 - Present</h5>
-        <p><em>Experion, New York, NY </em></p>
-        <ul>
-            <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-            <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-            <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-            <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-        </ul>
+            <h4>Développeur Junior</h4>
+            <h5>2023 - Present</h5>
+            <p><em><a href="#">KemboCorp</a>&copy;, Kinshasa, RDC </em></p>
+            <ul>
+                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+            </ul>
         </div>
         <div class="resume-item">
-        <h4>Graphic design specialist</h4>
-        <h5>2017 - 2018</h5>
-        <p><em>Stepping Stone Advertising, New York, NY</em></p>
-        <ul>
-            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-            <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-        </ul>
+            <h4>Développeur Débutant</h4>
+            <h5>2020 - 2021</h5>
+            <p><em>GwothSoft, Kinshasa, RDC</em></p>
+            <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            </ul>
+        </div>
+        <div class="resume-item">
+            <h4>Stade académique</h4>
+            <h5>2022</h5>
+            <p><em>D.G.RAD, Kinshasa, RDC</em></p>
+            <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+            </ul>
         </div>
     </div>
     </div>
