c4589dbdfe9ef386b62c554238eb0cea16c04d30
MichMav <michmav28@gmail.com>
2023-04-05 15:45:25 +0200

    fix the about

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
 
     <div class="section-title">
-    <h2>About</h2>
+    <h2>A propos</h2>
     <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
     </div>
 
@@ -12,7 +12,7 @@
         <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
     </div>
     <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-        <h3>UI/UX Designer &amp; Web Developer.</h3>
+        <h3>Designer UI/UX &amp; Analyste Programmeur.</h3>
         <p class="fst-italic">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua.
@@ -20,18 +20,18 @@
         <div class="row">
         <div class="col-lg-6">
             <ul>
-            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Anniversaire:</strong> <span>28 Février</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Internet:</strong> <span>www.michmav.000webhost.com</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Télephone:</strong> <span>+243 973 385 722</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Ville:</strong> <span>Kinshasa, RDC</span></li>
             </ul>
         </div>
         <div class="col-lg-6">
             <ul>
-            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>24</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Diplôme:</strong> <span>BAC +4</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Courriel:</strong> <span>michmav28@.com</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Disponible</span></li>
             </ul>
         </div>
         </div>
