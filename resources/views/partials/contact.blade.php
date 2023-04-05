88c7cf6f4fec6ed736ca58e553b7bb2f0de7868e
MichMav <michmav28@gmail.com>
2023-04-05 17:38:58 +0200

    fixed the contact

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -3,8 +3,7 @@
 <div class="container">
 
     <div class="section-title">
-    <h2>Contact</h2>
-    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        <h2>Contact</h2>
     </div>
 
     <div class="row" data-aos="fade-in">
@@ -13,20 +12,20 @@
         <div class="info">
         <div class="address">
             <i class="bi bi-geo-alt"></i>
-            <h4>Location:</h4>
-            <p>A108 Adam Street, New York, NY 535022</p>
+            <h4>Localisation :</h4>
+            <p>39 Rue de kimbangu, Kinshasa, RD-Congo </p>
         </div>
 
         <div class="email">
             <i class="bi bi-envelope"></i>
-            <h4>Email:</h4>
-            <p>info@example.com</p>
+            <h4>Email :</h4>
+            <p>michmav28@gmail.com</p>
         </div>
 
         <div class="phone">
             <i class="bi bi-phone"></i>
-            <h4>Call:</h4>
-            <p>+1 5589 55488 55s</p>
+            <h4>Appel :</h4>
+            <p>+243 9733 85 722</p>
         </div>
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
@@ -38,16 +37,16 @@
         <form action="forms/contact.php" method="post" role="form" class="php-email-form">
         <div class="row">
             <div class="form-group col-md-6">
-            <label for="name">Your Name</label>
+            <label for="name">Votre Nom</label>
             <input type="text" name="name" class="form-control" id="name" required>
             </div>
             <div class="form-group col-md-6">
-            <label for="name">Your Email</label>
+            <label for="name">Adresse Email</label>
             <input type="email" class="form-control" name="email" id="email" required>
             </div>
         </div>
         <div class="form-group">
-            <label for="name">Subject</label>
+            <label for="name">Sujet</label>
             <input type="text" class="form-control" name="subject" id="subject" required>
         </div>
         <div class="form-group">
@@ -55,11 +54,11 @@
             <textarea class="form-control" name="message" rows="10" required></textarea>
         </div>
         <div class="my-3">
-            <div class="loading">Loading</div>
+            <div class="loading">Chargement</div>
             <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
+            <div class="sent-message">Votre message a été envoyer. Merci!</div>
         </div>
-        <div class="text-center"><button type="submit">Send Message</button></div>
+        <div class="text-center"><button type="submit">Envoyer message</button></div>
         </form>
     </div>
 
